1acc29e43580f9cd50be74812f2bcc865067e551
unknown <shyleshraj2016@gmail.com>
2025-02-07 16:19:07 +0530

    adding test cases

--- jest.config.js
+++ jest.config.js
@@ -3,7 +3,7 @@ module.exports = {
   testEnvironment: 'jsdom',
   transform: {
     '^.+\\.tsx?$': 'ts-jest',
-    '^.+\\.css$': 'jest-transform-stub', // Add this line to mock CSS files
+    '^.+\\.css$': 'jest-transform-stub',
   },
   setupFilesAfterEnv: ['<rootDir>/src/setupTests.ts'],
   transformIgnorePatterns: ['node_modules/(?!@some-package)'],
